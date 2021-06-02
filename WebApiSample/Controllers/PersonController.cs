c135b965404c493a450de7cf2d0ca7a759e08e7e
Akhil mon <akhilmon@hotmail.com>
2021-06-02 10:16:34 +0530

    EF Working Code

--- WebApiSample/Controllers/PersonController.cs
+++ WebApiSample/Controllers/PersonController.cs
@@ -4,44 +4,38 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApiSample.DataSource;
 using WebApiSample.Models;
 
 namespace WebApiSample.Controllers
 {
     public class PersonController : ApiController
     {
+        StudentSystemEntities sse = new StudentSystemEntities();
 
-        public Person GetMyName()
+        [HttpPost]
+        public Student SaveStudent(Student student)
         {
+            Student std = new Student();
+            std.ID = student.ID;
+            std.Name = student.Name;
+            std.Malayalam = student.Malayalam;
+            std.English = student.English;
+            std.Hindi = student.Hindi;
 
-            Person p1 = new Person();
-            p1.ID = 1;
-            p1.Name = "Akhil";
+            sse.Students.Add(std);
+            sse.SaveChanges();
 
-            return p1;
+            return std;
         }
-
-        public List<Person> GetAllPersons()
+        public bool SavePerson(DataSource.Person person)
         {
-            List<Person> persons = new List<Person>();
+            DataSource.Person tempPerson = new DataSource.Person();
+            tempPerson.Id = person.Id;
+            tempPerson.Name = person.Name;
+            tempPerson.Email = person.Email;
 
-            Person p1 = new Person();
-            p1.ID = 1;
-            p1.Name = "Akhil";
-
-            Person p2 = new Person();
-            p2.ID = 2;
-            p2.Name = "Sachin";
-
-            Person p3 = new Person();
-            p3.ID = 3;
-            p3.Name = "Ann";
-
-            persons.Add(p1);
-            persons.Add(p2);
-            persons.Add(p3);
-
-            return persons;
+            return true;
         }
     }
 }
